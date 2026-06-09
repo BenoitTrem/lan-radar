d0fde9a4658a696e41d1a88251d49a924ed56d59
BenoitTrem <2027678@cegepoutaouais.qc.ca>
2026-06-08 20:56:45 -0400

    Fix the icon error.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Static export for Electron production build
-  output: process.env.ELECTRON_BUILD ? "export" : undefined,
-  images: { unoptimized: true },
-
-  // So Next.js asset paths work from file:// in Electron
-  assetPrefix: process.env.ELECTRON_BUILD ? "./" : "",
+  output: "export",
   trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
